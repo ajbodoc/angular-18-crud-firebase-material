2e3483e898856349bc2b82caa1b44949ca0481a8
Antonio Jorge Bodoc <ajbodoc@yahoo.com>
2024-11-16 19:14:30 -0300

    primer commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  firebase:{}
+  firebase:{
+    apiKey: "AIzaSyCcecDNAiuUTBMycmaOM0vXgWpQkxQkvMw",
+    authDomain: "firengmat.firebaseapp.com",
+    projectId: "firengmat",
+    storageBucket: "firengmat.firebasestorage.app",
+    messagingSenderId: "323365360800",
+    appId: "1:323365360800:web:6349aec30461b969ef25c5",
+    measurementId: "G-L2M3FRV82G"
+  }
 };
